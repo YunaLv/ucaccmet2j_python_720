ad2ee0c5d9659ddc447f4dbed066cb95a00b2d2b
Yuna Luisetto Vanbergen <yunamarbay@gmail.com>
2025-01-14 17:26:41 +0100

    exercise 3

--- relative_monthly_precipitation.py
+++ relative_monthly_precipitation.py
@@ -1,40 +1,52 @@
 import json
+import csv
+
+with open('stations.csv') as file:
+     stations =list(csv.DictReader(file))
+
+print(stations)
+
 
 with open("precipitation.json", encoding='utf-8') as file:
         content = json.load(file)
 
-dictio_seattle={}
+results = {}
+for station in stations:
+    all_dates='01 02 03 04 05 06 07 08 09 10 11 12'.split()
+    precipi_per_month={}
 
+    for date in all_dates:
+        precipi_per_month[date] = 0
 
-all_dates='01 02 03 04 05 06 07 08 09 10 11 12'.split()
-precipi_per_month={}
+    precipi_total=0
 
-for date in all_dates:
-      precipi_per_month[date] = 0
+    total_precipitation= 0
+    print(precipi_per_month)
+    for lab in content:
+        if lab ['station']==station['Station']:   
+            precipi_total += lab['value'] 
+            for date in all_dates:
+                if lab['date'].startswith(f'2010-{date}'):
+                        precipi_per_month[date] += lab['value']
 
-precipi_per_month['total']=0
+    print(precipi_per_month)
 
-total_precipitation= 0
-print(precipi_per_month)
-for lab in content:
-     if lab ['station']=='GHCND:US1WAKG0038':   
-          precipi_per_month['total'] += lab['value'] 
-          for date in all_dates:
-               if lab['date'].startswith(f'2010-{date}'):
-                    precipi_per_month[date] += lab['value']
+    relative_precipi = {}
+    for date in all_dates:
+        relative_precipi[date] = precipi_per_month[date]/precipi_total
 
-print(precipi_per_month)
+    print(relative_precipi)
+            
+    results[station["Location"]]={
+         "precipitation_per_month" :precipi_per_month,
+         "relative_precipitation":relative_precipi,
+         'total_precipitation':precipi_total
 
-for date in all_dates:
-     precipi_per_month[date] = precipi_per_month[date]/precipi_per_month['total']
-
-print(precipi_per_month)
-        
-
+    }
 
 
 with open('results.json', 'w', encoding='utf-8') as file:
-    json.dump(precipi_per_month, file,indent=4)
+    json.dump(results, file,indent=4)
 
 
 
